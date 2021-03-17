6d7746cd60476738a5b3185d3c5fdc8d710ff973
LiuXianfa <745024471@qq.com>
2021-03-17 14:56:32 +0800

    构造方法 循环依赖

--- spring-example/src/test/java/com/atguigu/test/circularReference/useConstructor/CircularDependencyTest.java
+++ spring-example/src/test/java/com/atguigu/test/circularReference/useConstructor/CircularDependencyTest.java
@@ -3,6 +3,7 @@ package com.atguigu.test.circularReference.useConstructor;
 import com.atguigu.circularReference.useConstructor.CircularReferenceConfig;
 
 import org.junit.Test;
+import org.springframework.beans.factory.UnsatisfiedDependencyException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -13,7 +14,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class CircularDependencyTest {
 
 
-    @Test
+    /**
+     * 构造方法 循环依赖
+     *
+     * 如果想要看异常信息,则把 expected = UnsatisfiedDependencyException.class 去掉.
+     */
+    @Test(expected = UnsatisfiedDependencyException.class)
     public void test() {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(CircularReferenceConfig.class);
         applicationContext.close();
